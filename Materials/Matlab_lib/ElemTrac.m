894ef4b235093e318dd07f725cc448d592ef37bd
NTU-CAE-David <r11521614@g.ntu.edu.tw>
2023-10-03 15:03:03 +0800

    Finish ElemTrac.m

--- Materials/Matlab_lib/ElemTrac.m
+++ Materials/Matlab_lib/ElemTrac.m
@@ -17,10 +17,10 @@ function rel = ElemTrac(itrac,coor,conn,trac)
         x1b = coor(1,conn(1,iel));
         x2b = coor(2,conn(1,iel));
     end
-    lel = ...;
-        rel = zeros(4,1);
+    lel = sqrt((x1a-x1b)^2+(x2a-x2b)^2);
+    rel = zeros(4,1);
     rel(1) = trac(3)*lel / 2;
     rel(2) = trac(4)*lel / 2;
-    .
-    .
+    rel(3) = rel(1);
+    rel(4) = rel(2);
 end
